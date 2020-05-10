c5399948aa64c7b949c06af94254744745e5c8ee
savi <shambhavishandilya01@gmail.com>
2020-05-10 23:59:38 +0530

    Added Multiple lines feature

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ router.post("/check", (req, res) => {
 var negative = new Array();
 var positive_data;
 var negative_data;
+var pos_count =0;
+var neg_count = 0;
+var neu_count = 0;
+var array_entry = new Array();
 try {  
     positive_data = fs.readFileSync('positive_words.txt', 'utf8');
     negative_data = fs.readFileSync('negative_words.txt', 'utf8');   
@@ -19,21 +23,31 @@ positive.pop();
 negative = negative_data.split(/\n|\r/g);
 negative.pop();
 var string = entry.toLowerCase();
-var final;
-let resultn = negative.some(o => string.includes(o));
-let resultp = positive.some(o => string.includes(o));
+array_entry = string.split('\n');
+var final = new Array();
+
+for(i=0; i<array_entry.length;i++)
+{
+let resultn = negative.some(o => array_entry[i].includes(o));
+let resultp = positive.some(o => array_entry[i].includes(o));
 if(resultn)
 {
-	final = 'Negative Review';
+	final[i] = 'Negative Review';
+	neg_count++;
 }
 else if(resultp)
 {
-	final = 'Positive Review';
+	final[i] = 'Positive Review';
+	pos_count++;
 }
 else
-final = 'Neutral Review';
+{
+final[i] = 'Neutral Review';
+neu_count++;
+}
+}
 
-res.render("check",{final:final , entry:entry});
+res.render("check",{final:final , entry:array_entry , pos : pos_count , neg : neg_count , neu : neu_count});
 });
 module.exports = router
 
